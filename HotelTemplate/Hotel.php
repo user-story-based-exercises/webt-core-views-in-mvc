eb50a7da1a38594af226448dfd9d0dfbca26b8fd
aswa <alexander.nems@gmail.com>
2024-03-19 09:29:13 +0100

    done

--- HotelTemplate/Hotel.php
+++ HotelTemplate/Hotel.php
@@ -5,19 +5,21 @@ class Hotel {
     private string $description;
     private string $address;
 
+    private string $src;
+
     /**
      * @param string $name
      * @param string $description
      * @param string $address
      */
-    public function __construct(string $name, string $description, string $address)
+    public function __construct(string $name, string $description, string $address, string $src)
     {
         $this->name = $name;
         $this->description = $description;
         $this->address = $address;
     }
 
-
+    
     public function getName(): string
     {
         return $this->name;
--- HotelTemplate/updatedEngine.php
+++ HotelTemplate/updatedEngine.php
@@ -1,14 +1,22 @@
 <?php
 
-require_once 'hotelContainer.php';
+require_once 'Hotel.php';
 
-$hotel = new Hotel('Sonnenscheinhotel', 'Genieße einen herrlichen Aufenthalt am Neusiedlersee', 'Neusiedlersee 1, 0000');
+$hotel1 = new Hotel('Sonnenscheinhotel', 'Genieße einen herrlichen Aufenthalt am Neusiedlersee', 'Neusiedlersee 1, 0000');
+$hotel2 = new Hotel('Internat der Golf-HAK für Richkids', 'Im Südburgenland ist alles besser', 'Südlich vom Sieggrabener sattel 5, 0000');
 
-$hotelContainer = new HotelContainer();
 
-$hotelContainer->addDivText($hotel);
+$hotels = array($hotel1, $hotel2);
+$engineParam = "";
+
+foreach ($hotels as $hotel) {
+    $engineParam .= file_get_contents("hotelTemplate.html");
+    $engineParam = str_replace('{name}', $hotel->getName(), $engineParam);
+    $engineParam = str_replace('{description}', $hotel->getDescription(), $engineParam);
+    $engineParam = str_replace('{address}', $hotel->getAddress(), $engineParam);
+}
 
 $hotelTemplate = file_get_contents("hotelpageTemplate.html");
-$hotelTemplate = str_replace('{placeholder}', $hotelContainer->getHotelDivs(), $hotelTemplate);
+$hotelTemplate = str_replace('{placeholder}', $engineParam, $hotelTemplate);
 
 echo $hotelTemplate;
